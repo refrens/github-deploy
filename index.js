f04ca7358069cd554d9bb9b27049ac6aef276aa9
Mohit <mayank.mohit@gmail.com>
2023-05-27 15:20:57 +0530

    feat: initial commit

---
+++ index.js
@@ -0,0 +1,46 @@
+#!/usr/bin/node
+
+const { exec } = require('child_process');
+const { program } = require('commander');
+
+const { Octokit } = require('@octokit/core');
+
+const octokit = new Octokit({ auth: process.env.GITHUB_TOKEN });
+
+/**
+ * Executes a shell command and return it as a Promise.
+ * @param cmd {string}
+ * @return {Promise<string>}
+ */
+function sh(cmd) {
+  return new Promise((resolve, reject) => {
+    exec(cmd, (error, stdout) => {
+      if (error) {
+        reject(error);
+      }
+      resolve(stdout.trim());
+    });
+  });
+}
+
+program
+  .option('-e, --environment <environment>', 'Deployment environment')
+  .action(async (options) => {
+    const branch = await sh('git rev-parse --abbrev-ref HEAD');
+    const origin = await sh('git remote get-url origin');
+    let [base, parent] = origin.split('/').reverse();
+    base = base.replace(/\.git$/, '');
+    parent = parent.split(':').pop();
+    const { environment = 'production' , ref = branch, owner = parent, repo = base } = options;
+    const resp = await octokit.request('POST /repos/{owner}/{repo}/deployments', {
+      owner,
+      repo,
+      ref,
+      auto_merge: false,
+      required_contexts: [],
+      environment,
+      description: `Deploy request for ref:${ref} on ${environment}`,
+    });
+    process.exit();
+  });
+program.parse(process.argv);
